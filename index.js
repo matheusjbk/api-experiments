e198d80082d86f90210e20f9c518e5c86adb85a1
Matheus Keher <matheusjbk@gmail.com>
2023-02-15 11:07:08 -0300

    Criação API

--- index.js
+++ index.js
@@ -3,13 +3,50 @@ const app = express()
 
 const conf = require('dotenv').config().parsed;
 
-console.log(conf);
+// console.log(conf);
 
 const port = conf.HTTPPORT;
 
-app.get('/', (req, res) => {
-  res.send('Olá Mundo!')
-})
+// Objeto que será executado quando houver uma requisição.
+const controller = {
+  resJson: async (req, res) => {
+    
+    // Lista com alguns atributos úteis da requisição (req) HTTP.
+    data = {
+      "method": req.method,
+      "url": req.url,
+      "baseURL": req.baseURL,
+      "query": res.query,
+      "params": req.params,
+      "body": req.body,
+      "headers": req.headers
+    }
+
+    // Envia JSON com os dados acima para o cliente, como texto plano.
+    // res.send(data);
+
+    // Envia JSON com os dados acima para o cliente, como texto plano JSON.
+    res.json(data);
+  }
+}
+
+// Recebe os dados do body HTTP e valida em JSON -> POST e PUT.
+const bodyParser = require('body-parser').json();
+
+// Rota para GET -> getAll() -> Recebe, por exemplo, todos os registros.
+app.get('/', controller.resJson);
+
+// Rota para GET -> get(id) -> Recebe apenas o registro identificando.
+app.get('/:id', controller.resJson);
+
+// Rota para DELETE -> delete(id).
+app.delete('/:id', controller.resJson);
+
+// Rota para POST -> post() -> bodyParser (no hook) é usado para garantir a chegada de um JSON.
+app.post('/', bodyParser, controller.resJson);
+
+// Rota para PUT -> put(id) -> bodyParser (no hook) é usado para garantir a chegada de um JSON.
+app.put('/:id', bodyParser, controller.resJson);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
